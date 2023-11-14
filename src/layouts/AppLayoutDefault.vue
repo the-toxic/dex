391238aa17f2fbd98b9979bef1c02cd873dababe
the-toxic <l2world12@gmail.com>
2023-11-14 15:36:29 +0400

    Fix call search dialog

--- src/layouts/AppLayoutDefault.vue
+++ src/layouts/AppLayoutDefault.vue
@@ -55,7 +55,7 @@
         <v-btn v-if="userStore.logged" @click="showSearch" rounded variant="text" class="text-body-2">
           <v-icon color="#B3B5BD" class="pr-4" alt="Show Search">mdi-magnify</v-icon>
           Search
-          <span class="py-0 px-3 ml-3 border rounded text-disabled text-caption">Cmd + K</span>
+          <span class="py-0 px-3 ml-3 border rounded text-disabled text-caption">⌘ + K</span>
         </v-btn>
 
         <!-- Menu open icon for mobile -->
@@ -234,7 +234,6 @@ import { ref, computed, onMounted, onUnmounted } from "vue";
   })
 
   function searchShortCutHandler(event) {
-    console.log(event)
     if ((event.metaKey || event.ctrlKey) && event.code === 'KeyK') { // event.key === '/' ; event.code === 'Slash'
       event.preventDefault()
       showSearch()
